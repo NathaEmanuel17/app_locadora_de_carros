b3180cc892d8c3464f5d8415e8d43fbcf0dab6c3
Natha <nathaemanuel.eng@gmail.com>
2024-01-07 15:53:26 -0300

    second commit

---
+++ app/Http/Controllers/MarcaController.php
@@ -0,0 +1,179 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Storage;
+use App\Models\Marca;
+use App\Repositories\MarcaRepository;
+use Illuminate\Http\Request;
+
+class MarcaController extends Controller
+{
+
+    public function __construct(Marca $marca)
+    {
+        $this->marca = $marca;
+    }
+
+    /**
+     * 
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+
+        $marcaRepository = new MarcaRepository($this->marca);
+
+        if ($request->has('atributos_modelos')) {
+            $atributos_modelos = 'modelos:id,' . $request->atributos_modelos;
+            $marcaRepository->selectAtributosRegistrosRelacionados($atributos_modelos);
+        } else {
+            $marcaRepository->selectAtributosRegistrosRelacionados('modelos');
+        }
+
+        if ($request->has('filtro')) {
+            $marcaRepository->filtro($request->filtro);
+        }
+
+        if ($request->has('atributos')) {
+            $marcaRepository->selectAtributos($request->atributos);
+        }
+        
+        return response()->json($marcaRepository->getResultado(), 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+
+        $request->validate($this->marca->rules(), $this->marca->feedback());
+        //stateless: precisa passar o Accept no header da requisição
+
+        $image = $request->file('imagem');
+        $imagem_urn = $image->store('imagens', 'public');
+
+        $marca = $this->marca->create([
+            'nome' => $request->nome,
+            'imagem' => $imagem_urn
+        ]);
+
+        return response()->json($marca, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  Integer
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $marca = $this->marca->with('modelos')->find($id);
+
+        if ($marca === null) {
+            return response()->json(['erro' => 'Recurso pesquisado não existe'], 404); // json
+        }
+
+        return response()->json($marca, 200);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Marca  $marca
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Marca $marca)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  Integer
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $marca = $this->marca->find($id);
+
+        if ($marca === null) {
+            return response()->json(['erro' => 'Impossivel realizar a solicitação, o recurso solicitado não existe'], 404);
+        }
+
+        if ($request->method() === 'PATCH') {
+
+            $regrasDinamicas = array();
+
+            foreach ($marca->rules() as $input => $regras) {
+
+                if (array_key_exists($input, $request->all())) {
+                    $regrasDinamicas[$input] = $regras;
+                }
+            }
+
+            $request->validate($regrasDinamicas, $marca->feedback());
+        } else {
+
+            $request->validate($marca->rules(), $marca->feedback());
+        }
+
+        //remove o arquivo antigo caso um novo arquivo tenha sido enviado no request
+        if ($request->file('imagem')) {
+            Storage::disk('public')->delete($marca->imagem);
+        }
+
+        $image = $request->file('imagem');
+        $imagem_urn = $image->store('imagens', 'public');
+
+        //preencher o objeto $marca com os dados do request
+        $marca->fill($request->all());
+        $marca->imagem = $imagem_urn;
+
+        $marca->save();
+
+        return response()->json($marca, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  Integer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $marca = $this->marca->find($id);
+
+        if ($marca === null) {
+            return response()->json(['erro' => 'Impossivel realizar a solicitação, o recurso solicitado não existe'], 404);
+        }
+
+        //remove o arquivo antigo caso um novo arquivo tenha sido enviado no request
+
+        Storage::disk('public')->delete($marca->imagem);
+
+        $marca->delete();
+        return response()->json(['msg' => 'A marca foi removida com sucesso!'], 200);
+    }
+}
---
+++ resources/js/components/Table.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <table class="table table-hover">
+        <thead>
+            <tr>
+                <th v-for="t, key in titulos">{{ t }}</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="m in dados" :key="m.id">
+                <th scope="row">{{ m.id }}</th>
+                <td>{{m.nome}}</td>
+                <td><img :src="'/storage/'+m.imagem" width="30"></td>
+            </tr>
+        </tbody>
+    </table>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['dados', 'titulos']
+}
+</script>
